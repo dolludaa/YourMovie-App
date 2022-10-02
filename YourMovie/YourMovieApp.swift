cc213ca9c5cf5c81fc34a0537e58b94dd7069626
dolludaa <111228178+dolludaa@users.noreply.github.com>
2022-10-02 23:33:34 +0300

    Add files via upload

---
+++ YourMovie/YourMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourMovieApp.swift
+//  YourMovie
+//
+//  Created by Людмила Долонтаева on 02.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct YourMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
