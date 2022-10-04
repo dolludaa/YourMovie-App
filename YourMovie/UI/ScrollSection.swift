c796a606d9fef622a41077377408a7325bd9b5bf
dolludaa <dolontaeva00@gmail.com>
2022-10-05 00:51:18 +0300

    ScrollSection

---
+++ YourMovie/UI/ScrollSection.swift
@@ -0,0 +1,20 @@
+//
+//  ScrollSection.swift
+//  YourMovie
+//
+//  Created by Людмила Долонтаева on 05.10.2022.
+//
+
+import SwiftUI
+
+struct ScrollSection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScrollSection_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrollSection()
+    }
+}
