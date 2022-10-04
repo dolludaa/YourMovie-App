993187dd9a930731364f8dff794e60763d2c2f0e
dolludaa <dolontaeva00@gmail.com>
2022-10-05 00:51:34 +0300

    ScrollSection

--- YourMovie/UI/ScrollSection.swift
+++ YourMovie/UI/ScrollSection.swift
@@ -1,15 +1,34 @@
-//
-//  ScrollSection.swift
-//  YourMovie
-//
-//  Created by Людмила Долонтаева on 05.10.2022.
-//
-
 import SwiftUI
 
 struct ScrollSection: View {
+    @State var title: String = "Now Playing"
+    
+    @State var posters: [String] = ["poster1", "poster2", "poster3", "poster4", "poster5", "poster6"]
+        
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Text(title)
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding(.horizontal, 20)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 20.0) {
+                    ForEach(posters.indices, id: \.self) { index in
+                        NavigationLink {
+//                            BookingView()
+                        } label: {
+                            Image(posters[index])
+                                .resizable()
+                                .frame(width: 100, height: 130)
+                                .cornerRadius(20)
+                        }
+                    }
+                }
+                .offset(x: 20)
+                .padding(.trailing, 40)
+            }
+        }
     }
 }
 
